6c7474c411d44d918059cf4e2a80a0667fd6e61f
Bence Fabian <begnoc@gmail.com>
2018-01-02 00:01:12 +0100

    initial commit

---
+++ asm.py
@@ -0,0 +1,37 @@
+from instruction import unpack16, jsr
+
+# data Instruction
+###
+# Word16 -> (Asm, Word16)
+
+# data Asm
+###
+# { label :: Maybe String
+# , address :: Word16
+# , data :: List (Either Word8 String)
+
+
+def asm(start_address, instructions):
+    '''Word16 -> List Instruction -> List Word8'''
+    output = []
+    program_counter = start_address
+    for instruction in instructions:
+        (new_data, new_counter) = instruction(program_counter)
+        output.append(new_data)
+        program_counter += new_counter
+    return fix(output)
+
+
+def fix(asm):
+    '''List Asm -> List Word8'''
+    def extract(addr):
+        inst = next(filter(lambda i: i['label'] == addr, asm))
+        return unpack16(inst['address'])
+    out = []
+    for a in asm:
+        for byte in a['data']:
+            if isinstance(byte, str):
+                out.extend(extract(byte))
+            else:
+                out.append(byte)
+    return out
---
+++ instruction.py
@@ -0,0 +1,29 @@
+def unpack16(word):
+    # TODO: byte order
+    return [word & 0xff, word >> 8]
+
+
+class Instruction:
+
+    def __call__(self, addr):
+        (length, data) = self.get_data()
+        asm = {'label': self.label,
+               'address': addr,
+               'data': data}
+        new_addr = addr+length
+        return (asm, new_addr)
+
+
+class jsr(Instruction):
+
+    def __init__(self, jump_to, label=None):
+        self.jump_to = jump_to
+        self.label = label
+
+    def get_data(self):
+        out = [0x20]
+        where = [self.jump_to]
+        if not isinstance(self.jump_to, str):
+            where = unpack16(self.jump_to)
+        out.extend(where)
+        return (3, out)
